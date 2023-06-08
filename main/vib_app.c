35028e74f47fcfaa0a3f1c336b750aafdb8319de
Nicholas H.R. Sims <nickhrsims@gmail.com>
2023-06-08 21:33:41 +0000

    fix: remove line-in pipeline bypass

--- main/vib_app.c
+++ main/vib_app.c
@@ -48,7 +48,7 @@ void app_main(void)
   // NOTE: Unused
   audio_board_handle_t board_handle __attribute__((unused)) = audio_board_init();
 
-  audio_hal_ctrl_codec(board_handle->audio_hal, AUDIO_HAL_CODEC_MODE_LINE_IN,
+  audio_hal_ctrl_codec(board_handle->audio_hal, AUDIO_HAL_CODEC_MODE_BOTH,
                        AUDIO_HAL_CTRL_START);
 
   ESP_LOGI(TAG, "[ 2 ] Create audio pipeline for playback");
