1f9852f44641a4cd4940e05ca93d522a68d49263
Nicholas H.R. Sims <nickhrsims@gmail.com>
2023-06-29 09:23:14 -0600

    feat: memory init module

--- main/main.c
+++ main/main.c
@@ -3,5 +3,9 @@
 /// @brief       Houses application entry-point.
 
 #include "vib_audio.h"
+#include "vib_memory.h"
 
-void app_main(void) { vib_audio_start(); }
+void app_main(void) {
+    vib_memory_init();
+    vib_audio_start();
+}
---
+++ main/vib_memory.h
@@ -0,0 +1,8 @@
+#ifndef VIB_MEMORY_H_
+#define VIB_MEMORY_H_
+
+#include "esp_err.h"
+
+esp_err_t vib_memory_init(void);
+
+#endif // VIB_MEMORY_H_
