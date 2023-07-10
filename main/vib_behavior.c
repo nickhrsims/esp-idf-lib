e2a156cbc661674cd5ab7262176a2688b6f98d13
Nicholas H.R. Sims <nickhrsims@gmail.com>
2023-07-10 15:29:57 +0000

    refactor: remove unused layers

--- main/vib_behavior.c
+++
@@ -1,5 +0,0 @@
-/// vib_behavior.h
-///
-/// @brief       Defines business logic for connecting audio controls.
-
-#include "vib_behavior.h"
--- main/vib_behavior.h
+++
@@ -1,8 +0,0 @@
-/// vib_behavior.h
-///
-/// @brief       Describes business logic for connecting audio controls.
-
-#ifndef VIB_BEHAVIOR_H_
-#define VIB_BEHAVIOR_H_
-
-#endif // VIB_BEHAVIOR_H_
